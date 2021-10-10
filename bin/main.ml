6948392afe9c44eadcb9d374bbc5edb981511f05
CJ Lee <cl2362@cornell.edu>
2021-10-10 17:08:21 -0400

    Moving rectangles

--- bin/main.ml
+++ bin/main.ml
@@ -6,10 +6,12 @@ let play_game name =
   open_graph " 500x500";
   set_window_title "OCamlMon";
   set_color black;
-  let pointx = 250 in
-  let pointy = 250 in
-  (* plot pointx pointy; *)
-  moveto 250 250;
+  let pointx = 0 in
+  let pointy = 0 in
+  plot 250 250;
+  (* plot 0 0; *)
+  foreground = red;
+  rmoveto 250 250;
   draw_rect 0 0 25 25;
   fill_rect 0 0 25 25;
   try
@@ -17,8 +19,18 @@ let play_game name =
       let st = wait_next_event [Key_pressed;] in
       synchronize();
       if st.keypressed then
-        if st.key = 'q' then raise Exit;
-        if st.key <> 'q' then moveto (pointx + 100) pointy;
+        if st.key = 'q' then raise Exit else
+          let (a,b) = current_point() in
+            if st.key = 'w' then 
+              moveto a (b+25);
+            if st.key = 'a' then
+              moveto (a-25) b;
+            if st.key = 's' then
+              moveto (a) (b-25);
+            if st.key = 'd' then
+              moveto (a+25) (b);
+            let (a,b) = current_point() in
+              draw_rect (a) (b) 25 25;
     done
   with Exit -> close_graph ()
 
