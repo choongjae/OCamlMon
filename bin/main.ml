6f3cc8dcdd5a9cb27e51e49e3ccbc169c43df47a
CJ Lee <cl2362@cornell.edu>
2021-10-10 18:29:34 -0400

    Grid initialization

--- bin/main.ml
+++ bin/main.ml
@@ -2,24 +2,36 @@
 #require "ANSITerminal";;
 open Graphics
 
+
+
 let play_game name =
-  open_graph " 500x500";
+  open_graph " 525x525";
   set_window_title "OCamlMon";
-  set_color black;
+  set_color green;
+
+  for x = 0 to 21 do
+    for y = 0 to 21 do
+      set_color green;
+      fill_rect (x*25) (y*25) 25 25;
+      set_color black;
+      draw_rect (x*25) (y*25) 25 25;
+    done;
+  done;
+
   let pointx = 0 in
   let pointy = 0 in
   plot 250 250;
   (* plot 0 0; *)
   foreground = red;
   rmoveto 250 250;
-  draw_rect 0 0 25 25;
-  fill_rect 0 0 25 25;
+  set_color black;
   try
     while true do
       let st = wait_next_event [Key_pressed;] in
       synchronize();
       if st.keypressed then
         if st.key = 'q' then raise Exit else
+        if st.key = 'c' then clear_graph () else
           let (a,b) = current_point() in
             if st.key = 'w' then 
               moveto a (b+25);
@@ -31,6 +43,7 @@ let play_game name =
               moveto (a+25) (b);
             let (a,b) = current_point() in
               draw_rect (a) (b) 25 25;
+              fill_rect (a) (b) 25 25;
     done
   with Exit -> close_graph ()
 
