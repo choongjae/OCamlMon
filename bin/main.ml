e5b7db153b7bd7dc07214f0ed01dc11830e81000
CJ Lee <cl2362@cornell.edu>
2021-10-10 16:11:47 -0400

    Primitive graphics

--- bin/main.ml
+++ bin/main.ml
@@ -4,7 +4,23 @@ open Graphics
 
 let play_game name =
   open_graph " 500x500";
-  set_window_title "OCamlMon"
+  set_window_title "OCamlMon";
+  set_color black;
+  let pointx = 250 in
+  let pointy = 250 in
+  (* plot pointx pointy; *)
+  moveto 250 250;
+  draw_rect 0 0 25 25;
+  fill_rect 0 0 25 25;
+  try
+    while true do
+      let st = wait_next_event [Key_pressed;] in
+      synchronize();
+      if st.keypressed then
+        if st.key = 'q' then raise Exit;
+        if st.key <> 'q' then moveto (pointx + 100) pointy;
+    done
+  with Exit -> close_graph ()
 
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
