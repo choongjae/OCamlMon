699d1733fba4d681e0644059aed4c456ae1faa0f
Sabrina Li <sl3324@cornell.edu>
2021-10-10 14:54:52 -0400

    made the main file

---
+++ bin/main.ml
@@ -0,0 +1,18 @@
+open Graphics
+let play_game name =
+  open_graph " 480x270"
+
+
+
+(** [main ()] prompts for the game to play, then starts it. *)
+let main () =
+  ANSITerminal.print_string [ ANSITerminal.red ]
+    "\n\nWelcome to the world of OCámlMon!";
+  print_endline
+    "Please enter your name: ";
+  print_string "> ";
+  match read_line () with
+  | exception End_of_file -> ()
+  | name -> play_game (name)
+(* Execute the game engine. *)
+let () = main ()
