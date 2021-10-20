bc25a30412abfd84991d4a7ca1a07c30821cd020
CJ Lee <cl2362@cornell.edu>
2021-10-20 00:49:19 -0400

    Cleans up main.ml
    - Modularizes code in main engine loop
    - Adds specifications for helper functions
    - Prototypes more features from graphics library like auto_synchronize

--- bin/main.ml
+++ bin/main.ml
@@ -1,15 +1,33 @@
+(* Use [utop -init bin/main.ml] to run (while in main directory) *)
 #require "Graphics";;
 #require "ANSITerminal";;
 open Graphics
 let mainWorldlist = []
 
-let draw_square x y = begin
-  set_color green;
+(** [draw_square (x, y) f b] draws a 25x25 pixel square with the bottom-left
+corner at [(x, y)], with a fill color [f] and border color [b]. *)
+let draw_square (x, y) fill border = begin
+  set_color fill;
   fill_rect x y 25 25;
-  set_color black;
+  set_color border;
   draw_rect x y 25 25;
 end
 
+(** [move (x, y) (u, v)] moves the character from its initial position of 
+[(x, v)] to [(u, v)]. Note that these coordinates correspond to the bottom-left
+corner of the grid square that the character is on. We also use auto_synchronize
+to batch the operations of filling in the old/new squares. *)
+let move (xinit, yinit) (xlast, ylast) = begin
+  moveto xlast ylast;
+  auto_synchronize false;
+  draw_square xlast ylast black black;
+  draw_square xinit yinit green black;
+  auto_synchronize true;
+  (* synchronize(); *)
+end
+
+(** [play_game n] begins running the main game loop with the [n] customization
+details that the user provided. *)
 let play_game name =
   open_graph " 525x525";
   set_window_title "OCamlMon";
@@ -17,52 +35,37 @@ let play_game name =
 
   for x = 0 to 21 do
     for y = 0 to 21 do
-      draw_square (x*25) (y*25)
+      draw_square (x*25) (y*25) green black
     done;
   done;
 
-  let pointx = 0 in
-  let pointy = 0 in
-  plot 250 250;
-  (* plot 0 0; *)
-  foreground = red;
-  rmoveto 250 250;
-  set_color black;
+  moveto 250 250;
   try
     while true do
       let st = wait_next_event [Key_pressed;] in
-      synchronize();
       if st.keypressed then
         if st.key = 'q' then raise Exit else
         if st.key = 'c' then clear_graph () else
           let (a,b) = current_point() in
-            if st.key = 'w' then 
-              moveto a (b+25);
-            if st.key = 'a' then
-              moveto (a-25) b;
-            if st.key = 's' then
-              moveto (a) (b-25);
-            if st.key = 'd' then
-              moveto (a+25) (b);
-            let (a,b) = current_point() in
-              draw_square (a-25) (b);
-              draw_square (a+25) (b);
-              draw_square (a) (b-25);
-              draw_square (a) (b+25);
-              draw_rect (a) (b) 25 25;
-              fill_rect (a) (b) 25 25;
+            match st.key with
+            | 'w' -> move (a, b) (a, b+25);
+            | 'a' -> move (a, b) (a-25, b);
+            | 's' -> move (a, b) (a, b-25);
+            | 'd' -> move (a, b) (a+25, b);
+            | _ -> ();
     done
-  with Exit -> close_graph ()
+  with Exit -> close_graph; ()
 
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
   ANSITerminal.print_string [ ANSITerminal.red ]
-    "\n\nWelcome to the world of OCámlMon!";
+    "\n\nWelcome to the world of OCámlMon! \n";
   print_endline
     "Please enter your name: ";
   print_string "> ";
   match read_line () with
   | exception End_of_file -> ()
   | name -> play_game (name)
+
 (* Execute the game engine. *)
 let () = main ()
