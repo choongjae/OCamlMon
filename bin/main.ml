9ce3e7ff915b30885688779945f1c071dae03156
choongjae <choongjae@hotmail.com>
2021-11-03 21:42:14 -0400

    Implements static data storage system with JSON
    - New dependency yojson for JSON parsing
    - New folder /data for holding JSONs
    - More specifications

--- bin/main.ml
+++ bin/main.ml
@@ -1,3 +1,4 @@
+open Yojson.Basic.Util
 open Graphics
 open Game
 open State
@@ -6,36 +7,41 @@ open Tile
 
 let mainWorldlist = []
 
-(*hi im sab*)
-let player = 
-  (*  1       2       3       4       5       6       7       8       9       10       11      12    13(m)    14      15      16      17      18      19      20      21      22      23      24     25 *)
-  [|
-    [|transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp|];
-    [|transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; transp; transp; black ; black ; black ; black ; black ; black ; black ; transp; transp; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; transp; transp; black ; black ; black ; black ; black ; black ; black ; transp; transp; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; transp; black ; red   ; red   ; red   ; red   ; red   ; red   ; red   ; black ; transp; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; black ; red   ; red   ; red   ; red   ; red   ; red   ; red   ; red   ; red   ; black ; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; black ; red   ; red   ; red   ; red   ; red   ; red   ; red   ; red   ; red   ; black ; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; black ; black ; black ; red   ; transp; transp; transp; transp; transp; red   ; black ; black ; black ; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; black ; black ; yellow; black ; black ; black ; black ; black ; black ; black ; yellow; black ; black ; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; black ; yellow; black ; yellow; yellow; yellow; yellow; yellow; yellow; yellow; yellow; yellow; black ; yellow; black ; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; black ; yellow; yellow; yellow; yellow; black ; yellow; yellow; yellow; black ; yellow; yellow; yellow; yellow; black ; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; black ; black ; yellow; transp; black ; yellow; yellow; yellow; black ; yellow; yellow; black ; black ; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; black ; black ; black ; yellow; yellow; red   ; red   ; red   ; yellow; yellow; black ; black ; black ; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; black ; yellow; yellow; black ; black ; black ; black ; black ; black ; black ; black ; yellow; yellow; yellow; black ; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; black ; yellow; yellow; black ; black ; black ; black ; black ; black ; black ; black ; yellow; yellow; yellow; black ; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; black ; black ; black ; red   ; red   ; black ; black ; black ; red   ; red   ; black ; black ; black ; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; black ; red   ; black ; black ; red   ; red   ; red   ; black ; black ; red   ; black ; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; black ; red   ; black ; black ; red   ; red   ; red   ; black ; black ; red   ; black ; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; black ; red   ; red   ; red   ; black ; black ; black ; red   ; red   ; red   ; black ; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; transp; black ; black ; black ; transp; transp; transp; black ; black ; black ; transp; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; transp; black ; black ; black ; transp; transp; transp; black ; black ; black ; transp; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp|];
-  [|transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp; transp|]
-  |]
+(** [parse_color c] is the Graphics library color type corresponding to an
+identically-named string [c]. Defaults to white if there is no match. *)
+let parse_color c =
+  let c = to_string c in
+  match c with
+  | "transp" -> transp
+  | "black" -> black
+  | "white" -> white
+  | "red" -> red
+  | "green" -> red
+  | "blue" -> blue
+  | "yellow" -> yellow
+  | "cyan" -> cyan
+  | "magenta" -> magenta
+  | _ -> white
+
+(** [parse_inner_list j] is the color list of [j], where [j] is a Yojson.Basic.t
+list of colors represented as strings, essentially for an inner-list of the 2D
+list from the JSON file. *)
+let rec parse_inner_list j =
+  match j with
+  | [] -> []
+  | h :: t -> parse_color h :: parse_inner_list t
+
+(** [parse_list j] is the color array list of the Yojson.Basic.t list [j],
+where [j] is a 2D list containing more Yojson.Basic.t lists of the color values
+for the sprite *)
+let rec parse_list j = 
+  match j with
+  | [] -> []
+  | h :: t -> (h |> to_list |> parse_inner_list |> Array.of_list) :: parse_list t
+
+let tsprite = "data/player.json" |> Yojson.Basic.from_file |> member "sprite"
+let spritelist = tsprite |> to_list
+let player = spritelist |> parse_list |> Array.of_list
 
 (** [draw_square (x, y) f b] draws a 25x25 pixel square with the bottom-left
 corner at [(x, y)], with a fill color [f] and border color [b]. *)
@@ -46,24 +52,34 @@ let draw_square (x, y) fill border = begin
   draw_rect x y 25 25;
 end
 
+(** [is_exit x y r] is whether or not the given coordinates [(x, y)] correspond
+to a valid exit in the given room [r]. *)
 let rec is_exit (x,y) = function
 | [] -> false
 | h :: t -> 
   if x == fst(h.coordinates) && y == snd(h.coordinates) then true
   else is_exit (x,y) t
 
+(** [helper r x y] is a helper function for [stay_or_exit]
+TODO: More descriptive name. *)
 let rec helper room (x,y) = function
 | [] -> room
 | h :: t -> 
   if (x == fst(h.coordinates) && y == snd(h.coordinates)) then h.name
   else helper room (x,y) t
 
+(** [stay_or_exit r x y] is the string for the room that the player should
+be in after moving, depending on their current room [r] and their current
+position [(x, y)] *)
 let stay_or_exit room (x, y) = begin
   let the_room = string_to_room room in
   let pos_exits = the_room.exits in
   helper room (x,y) pos_exits
 end
 
+(** [get_exit_start_pos x y] is the position of the player of the next room
+after taking an exit. For instance, if the player exits to the right of a room,
+their start position of the next room will be on the left. *)
 let get_exit_start_pos (x, y) = begin
   if x = -25 then (475, y)
   else if x = 500 then (0, y)
@@ -71,7 +87,8 @@ let get_exit_start_pos (x, y) = begin
   else (x, 0)
 end
 
-
+(** [draw_room arr] draws the tiles of the room array [arr] to the current
+graphics screen *)
 let draw_room room_array = begin
   for row = 0 to 20 do
     for col = 0 to 20 do
@@ -92,7 +109,8 @@ let move room (xinit, yinit) (xlast, ylast) sprite fill = begin
     print_endline "is exit";
   if (xlast >= 0 && ylast >= 0 && xlast <=500 && ylast <= 500) then
     print_endline "in bounds"; *)
-  if is_exit (xlast, ylast) (string_to_room room).exits || (xlast >= 0 && ylast >= 0 && xlast <=500 && ylast <= 500) then
+  if is_exit (xlast, ylast) (string_to_room room).exits ||
+    (xlast >= 0 && ylast >= 0 && xlast < 500 && ylast < 500) then
     (moveto xlast ylast;
     auto_synchronize false;
     draw_image sprite xlast ylast;
@@ -110,6 +128,8 @@ let move room (xinit, yinit) (xlast, ylast) sprite fill = begin
     update_state room (xinit, yinit) Walk
 end
 
+(** [test_print_poke n] prints a random Pokemon encounter depending on state [n]
+if it occurs. Using for testing purposes before implementing battle engine *)
 let test_print_poke next_state = begin
   let curr_room = get_current_room next_state in
   let curr_tile = get_tile (get_current_coord next_state) curr_room in
@@ -122,6 +142,8 @@ let test_print_poke next_state = begin
     | _ -> gen_poke_test
 end
 
+(** [play s] is the main game loop for running OCamlMon, where [s] represents
+the current state that the player is in. *)
 let rec play curr_state = 
   let player_sprite = make_image player in
     let fst_coord = (fst (get_current_coord curr_state)) in
